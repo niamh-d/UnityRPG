41666c045566753cc1a528af0022d629d5f673eb
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-08-11 11:44:53 +0300

    implemented new inpyt system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private PlayerInputSet _inputSet;
     private StateMachine stateMachine;
     public Player_IdleState idleState { get; private set; }
     public Player_MoveState moveState { get; private set; }
@@ -9,8 +10,24 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         stateMachine = new StateMachine();
+        _inputSet = new PlayerInputSet();
+        Debug.Log("Player Input Set Initialized");
+        _inputSet.Player.Movement.performed += ctx => Debug.Log("Movement Input: " + ctx.ReadValue<Vector2>());
+
         idleState = new Player_IdleState(this, stateMachine, "idle");
         moveState = new Player_MoveState(this, stateMachine, "move");
+    }
+
+    private void OnEnable()
+    {
+        _inputSet.Enable();
+
+        Debug.Log("Player Input Set Enabled");
+    }
+
+    private void OnDisable()
+    {
+        _inputSet.Disable();
     }
 
     private void Start()
